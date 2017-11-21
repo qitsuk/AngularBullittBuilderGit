64a816ee422ccf38e0427eff030f17e58dad89df
qiTsuk <kasper.andreasen@gmail.com>
2017-11-21 12:41:09 +0100

    Works better now!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,19 +18,37 @@ import { Component } from '@angular/core';
   </div>
 
   <input type="button" class="btn btn-primary" value="Next"
-    style="float: right; margin-right: 10px; height: 900px; width: 100px;" (click)="updateProgressBar()">
+    style="float: right; margin-right: 10px; height: 900px; width: 100px;" (click)="next()">
   <input type="button" class="btn btn-primary" value="Previous"
-    style="float: left; margin-left: 10px; height: 900px; width: 100px;" onClick="">
+    style="float: left; margin-left: 10px; height: 900px; width: 100px;" (click)="previous()">
   `
 })
 export class AppComponent {
   currentPage = 0;
   currentPercent = 0;
 
-  updateProgressBar() {
-    this.currentPage += 190;
-    this.currentPercent += 10;
-    $('#bike-builder-progress-bar')[0].innerHTML = '' + this.currentPercent + '%';
-    $('.progress-bar').animate({width: this.currentPage}, 100);
+  next() {
+    if (this.currentPage !== 1900) {
+      this.currentPage += 190;
+      this.currentPercent += 10;
+      this.updateProgressBar(this.currentPage, this.currentPercent);
+    } else {
+      alert('You are done!');
+    }
+  }
+
+  previous() {
+    if (this.currentPage !== 0) {
+      this.currentPage -= 190;
+      this.currentPercent -= 10;
+      this.updateProgressBar(this.currentPage, this.currentPercent);
+    } else {
+      alert('You can\'t go further back!!');
+    }
+  }
+
+  updateProgressBar(page, percent) {
+    $('#bike-builder-progress-bar')[0].innerHTML = '' + percent + '%';
+    $('.progress-bar').animate({width: page}, 100);
   }
 }
