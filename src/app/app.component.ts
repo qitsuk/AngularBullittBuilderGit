1df5823a71792e66fc7072d6d902c3bdbf5a2314
qiTsuk <kasper.andreasen@gmail.com>
2017-11-21 11:48:56 +0100

    Er så småt begyndt! Har fået lavet lidt småting. Ikke noget der virker endnu, men....

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:
+  `
+  <p></p>
+  <div class="progress">
+    <div
+      class="progress-bar
+      progress-bar-striped active"
+      role="progressbar"
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style="width: 90%">
+       {{currentPage}}%
+    </div>
+  </div>
+
+  <input type="button" class="btn btn-primary" value="Next"
+    style="float: right; margin-right: 10px; height: 750px; width: 100px;" onClick="">
+  <input type="button" class="btn btn-primary" value="Previous"
+    style="float: left; margin-left: 10px; height: 750px; width: 100px;" onClick="">
+  `
 })
 export class AppComponent {
-  title = 'app';
+  currentPage = 10;
 }
---
+++ src/app/page1.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+import { FormControl } from '@angular/forms';
+
+@Component({
+    selector: 'app-page-1',
+    template:
+    `
+    `
+})
+
+export class Page1Component {
+}
