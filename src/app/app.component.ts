e13f57cbc28566a30ca46a2224c99f80cfda8cfa
qiTsuk <kasper.andreasen@gmail.com>
2017-11-21 12:31:21 +0100

    Working Progress Bar - ish!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,36 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-root',
   template:
   `
-  <p></p>
-  <div class="progress">
+  <div class="progress"
+  style="margin-top: 10px; margin-left: 10px; margin-right: 10px;">
     <div
+      id="bike-builder-progress-bar"
       class="progress-bar
       progress-bar-striped active"
       role="progressbar"
       aria-valuemin="0"
       aria-valuemax="100"
-      style="width: 90%">
-       {{currentPage}}%
+      style="width: 0%">
     </div>
   </div>
 
   <input type="button" class="btn btn-primary" value="Next"
-    style="float: right; margin-right: 10px; height: 750px; width: 100px;" onClick="">
+    style="float: right; margin-right: 10px; height: 900px; width: 100px;" (click)="updateProgressBar()">
   <input type="button" class="btn btn-primary" value="Previous"
-    style="float: left; margin-left: 10px; height: 750px; width: 100px;" onClick="">
+    style="float: left; margin-left: 10px; height: 900px; width: 100px;" onClick="">
   `
 })
 export class AppComponent {
-  currentPage = 10;
+  currentPage = 0;
+  currentPercent = 0;
+
+  updateProgressBar() {
+    this.currentPage += 190;
+    this.currentPercent += 10;
+    $('#bike-builder-progress-bar')[0].innerHTML = '' + this.currentPercent + '%';
+    $('.progress-bar').animate({width: this.currentPage}, 100);
+  }
 }
