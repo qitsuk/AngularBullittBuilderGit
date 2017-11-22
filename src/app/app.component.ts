aeb221014adf9df3e54efcf7306c2452caea5b0c
CECILIE-PC\Cecilie <cecilie.l.karlsson@gmail.com>
2017-11-22 11:36:28 +0100

    playing around with css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,8 @@ import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html', 
+  templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
-  
 })
 
 export class AppComponent {
