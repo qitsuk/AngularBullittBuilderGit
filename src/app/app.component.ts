7906b1001a6dfdab3fe4fa1373a3abe80bb5d0af
CECILIE-PC\Cecilie <cecilie.l.karlsson@gmail.com>
2017-11-22 11:20:27 +0100

    app component css oprettet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,9 @@ import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html', 
+  styleUrls: ['app.component.css']
+  
 })
 
 export class AppComponent {
