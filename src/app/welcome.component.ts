83c7675cf0ea4dbf7f4c2ee781bb2ec37eec3bd0
CECILIE-PC\Cecilie <cecilie.l.karlsson@gmail.com>
2017-11-22 10:58:40 +0100

    created a css for welcome component

--- src/app/welcome.component.ts
+++ src/app/welcome.component.ts
@@ -5,7 +5,8 @@ import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-welcome',
-    templateUrl: 'welcome.component.html'
+    templateUrl: 'welcome.component.html',
+    styleUrls: ['welcome.component.css']
 })
 
 export class WelcomeComponent {
