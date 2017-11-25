556f5eaa8c4c2b1ac2030ab31d8248bf944274cd
qiTsuk <kasper.andreasen@gmail.com>
2017-11-25 15:22:46 +0100

    Drivetrain Buttons.

--- src/app/drivetrain.component.ts
+++ src/app/drivetrain.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AppComponent } from './app.component';
 
 @Component({
     selector: 'app-drivetrain-component',
@@ -7,5 +10,16 @@ import { Component } from '@angular/core';
 })
 
 export class DrivetrainComponent {
-
+    form: FormGroup;
+    constructor(private _router: Router, private _ac: AppComponent, fb: FormBuilder) {
+        this.form = fb.group({});
+    }
+    submit() {
+        this._router.navigate(['accessories']);
+        this._ac.updateProgressBar(40);
+    }
+    back() {
+        this._router.navigate(['color']);
+        this._ac.updateProgressBar(20);
+    }
 }
